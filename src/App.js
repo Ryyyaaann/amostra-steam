50e515f07f21946ef19087da9f8eb1a3c53a6fec
Ryyyaaann <ryan.s.franca.20@gmail.com>
2024-04-06 14:23:54 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import './App.css'
-import HellWorld from './Components/HelloWorld'
+import Navbar from './Components/Navbar'
 import QualSeuNome from './Components/QualSeuNome'
 import Pessoa from './Components/Pessoa'
 
 function App() {
-  const name = 'Leandro'
-  const nome = "Bolacho"
-
-  const newName = name.toLowerCase()
-
-
-  function soma(a, b){
-    return a+b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
   return (
     <div className="App">
-      <p>Olá, {newName}</p>
-      <p>Soma: {soma(1,2)}</p>
-      <img src ={url} alt="Minha Imagem"></img>
-      <HellWorld/>
-      <QualSeuNome nome="Bolacho"/>
-      <Pessoa
-        nome = "Biscoite" 
-        idade = "32"
-        profissao="acabar com a fome"
-        foto="https://via.placeholder.com/150"
-      />
+      
+      <Navbar/>
+      
       </div>
   );
 }
